d9581bdc8536dd009d4c8ea6b8e5fb6220c2b2ae
fxy05 <fxy05.07.26@qq.com>
2025-10-23 10:37:14 +0800

    第一天提交

---
+++ 1.1.c
@@ -0,0 +1,14 @@
+//20251030422
+//fxy05.07.26@qq.com
+//王鑫
+#include <stdio.h>
+
+int main()
+ {
+    float celsius = 0;
+    printf("请输入摄氏度:");
+    scanf("%f", &celsius);
+    float fahrenheit = celsius * 9 / 5 + 32;
+    printf("请输出华氏温度: %.1f\n", fahrenheit);
+    return 0;
+}
