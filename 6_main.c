fd4d96a119a998b5f4eff963ae44fbd03f20215f
fxy05 <fxy05.07.26@qq.com>
2025-10-30 21:00:44 +0800

    6_main.c

---
+++ 6_main.c
@@ -0,0 +1,32 @@
+//20251030422
+//fxy05.07.26@qq.com
+//王鑫
+#include <stdio.h>
+
+int main()
+ {
+    int nums[5];
+    int count = 0;
+    int num;
+    
+  
+    while (count < 5) 
+    {
+        scanf("%d", &num);
+        if (num % 2 == 0) 
+        {  
+            nums[count] = num;
+            count++;
+        }
+  
+    }
+    
+ 
+    for (int i = 0; i < 5; i++) 
+    {
+        printf("%d ", nums[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
