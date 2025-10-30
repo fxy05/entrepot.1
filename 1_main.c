e9dde7f3fbc751009e70c399dbcd20a5f75a66a9
fxy05 <fxy05.07.26@qq.com>
2025-10-30 20:54:46 +0800

    1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,13 +1,48 @@
-//202510304222
+//20251030422
 //fxy05.07.26@qq.com
 //王鑫
 #include <stdio.h>
 
-int main() {
-    float celsius = 0;
-    printf("请输入摄氏度:");
-    scanf("%f", &celsius);
-    float fahrenheit = celsius * 9 / 5 + 32;
-    printf("请输出华氏温度: %.1f\n", fahrenheit);
+
+int is_prime(int n) 
+{
+    if (n <= 1)
+    {
+        return 0;  
+    }
+    if (n == 2)
+    {
+        return 1; 
+    }
+    
+    
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0) 
+        {
+            return 0;  
+        }
+    }
+    return 1;  
+}
+
+int main()
+{
+    int n;
+    
+    
+    scanf("%d", &n);
+    
+   
+    if (is_prime(n))
+    {
+        printf("密钥安全，密码设置成功\n");
+    }
+     else
+    {
+        printf("密钥不安全，请重新输入\n");
+    }
+    
     return 0;
 }
+
