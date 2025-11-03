960f8e807705eef8eb3b84364f0a6bad11ead0b2
fxy05 <fxy05.07.26@qq.com>
2025-11-03 22:08:50 +0800

    1_main.c

--- 1_main.c
+++ 1_main.c
@@ -2,47 +2,29 @@
 //fxy05.07.26@qq.com
 //王鑫
 #include <stdio.h>
-
-
-int is_prime(int n) 
-{
-    if (n <= 1)
-    {
-        return 0;  
-    }
-    if (n == 2)
-    {
-        return 1; 
-    }
-    
-    
-    for (int i = 2; i < n; i++)
-    {
-        if (n % i == 0) 
-        {
-            return 0;  
-        }
-    }
-    return 1;  
-}
-
 int main()
 {
-    int n;
-    
-    
-    scanf("%d", &n);
-    
-   
-    if (is_prime(n))
+int arr[3][3];
+int i,j;
+for ( i = 0; i < 3; i++)
+{
+    char col[4];
+    scanf("%s",col);
+
+for ( j = 0; j < 3; j++)
+{
+   arr[i][j]=col[j]-'0';
+}
+}
+for (i = 0; i < 3; i++)
+{
+    for ( j = 0; j <3 ; j++)
     {
-        printf("密钥安全，密码设置成功\n");
+        printf("%d",arr[i][j]);
     }
-     else
-    {
-        printf("密钥不安全，请重新输入\n");
-    }
-    
-    return 0;
+    printf("\n");
 }
 
+return 0;
+
+}
