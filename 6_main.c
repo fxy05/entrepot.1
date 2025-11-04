6312a09125c3b1a2f47cb5a0bb1160a995e7e3cf
fxy05 <fxy05.07.26@qq.com>
2025-11-04 21:55:54 +0800

    6_main.c

--- 6_main.c
+++ 6_main.c
@@ -1,45 +1,30 @@
 //202510304222
 //fxy05.07.26@qq.com
 //王鑫
-#include <stdio.h>
-
-int array_sum(int arr[], int length) 
-{
-    int sum = 0;
-    for (int i = 0; i < length; i++) 
-    {
-        sum += arr[i];
+#include<stdio.h>
+int calculateSum(int arr[],int length){
+    int sum=0;
+    for(int i= 0;i< length;i++){
+    sum +=arr[i];
     }
     return sum;
 }
-
-int array_product(int arr[], int length) 
-{
-    int product = 1;
-    for (int i = 0; i < length; i++) 
-    {
-        product *= arr[i];
+int calculateProduct(int arr[],int length){
+    int product=1;
+    for(int i =0;i<length;i++){
+        product *=arr[i];
     }
     return product;
 }
-
-int main() 
+int main()
 {
     int arr[5];
-    
-    
-    printf("请输入5个整数：\n");
-    for (int i = 0; i < 5; i++) 
-    {
-        scanf("%d", &arr[i]);
+    for(int i = 0;i < 5;i++){
+        scanf("%d",&arr[i]);
     }
-    
-
-    int sum = array_sum(arr, 5);
-    int product = array_product(arr, 5);
-    
-    printf("数组元素的和：%d\n", sum);
-    printf("数组元素的积：%d\n", product);
-    
+    int sum =calculateSum(arr,5);
+    int product =calculateProduct(arr,5);
+    printf("%d %d\n",sum,product);
     return 0;
 }
+
