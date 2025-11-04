f64623217ba0314930ae9e615350d1b2d87519d1
fxy05 <fxy05.07.26@qq.com>
2025-11-04 21:52:01 +0800

    3_main.c

--- 3_main.c
+++ 3_main.c
@@ -2,33 +2,37 @@
 //fxy05.07.26@qq.com
 //王鑫
 #include <stdio.h>
-int main()
-{
-int arr[10];
-for (int i = 0; i < 10; i++)
-{
-    scanf("%1d",&arr[i]);
-}
-for (int i = 0; i < 9; i++)
-{
-    for (int j = 0; j <9-i ; j++)
+int main() {
+    int arr[10];
+    for (int i = 0; i < 10; i++) 
     {
-        if (arr[j+1]<arr[j])
-        {
-            int temp=arr[j];
-            arr[j]=arr[j+1];
-            arr [j+1]=temp ;
-        }   
+        scanf("%d", &arr[i]);
     }
-}
-for ( int i = 0; i < 10; i++)
-{
-    printf("%d ",arr[i]);
-}
-printf("\n");
+    for (int i = 0; i < 10 - 1; i++) 
+    {
+        for (int j = 0; j < 10 - 1 - i; j++) 
+        {
+            if (arr[j] > arr[j + 1]) 
+            {
+               
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < 10; i++) 
+    {
+        if (i == 9) 
+        {
+            printf("%d", arr[i]);
+        } 
+        else 
+        {
+            printf("%d ", arr[i]);
+        }
+    }
+    printf("\n");
 
-return 0;
-
-
-
+    return 0;
 }
