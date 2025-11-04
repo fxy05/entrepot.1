b214956ec21ab1ff5e87f6dc3df936d09aa61900
fxy05 <fxy05.07.26@qq.com>
2025-11-04 21:53:39 +0800

    5_main.c

--- 5_main.c
+++ 5_main.c
@@ -1,20 +1,15 @@
 //202510304222
 //fxy05.07.26@qq.com
 //王鑫
-#include <stdio.h>
-
-
-int arithmeticSeriesSum(int a1, int an, int step) 
+#include<stdio.h>
+int arithmeticSum(int a1,int an,int step)
 {
-    int n = (an - a1) / step + 1; 
-    return n * (a1 + an) / 2;      
+    int n=(an-a1)/step + 1;
+    return n*(a1 + an) / 2;
 }
-
 int main()
- {
-    
-    int sum = arithmeticSeriesSum(1, 100, 1);
-    printf("%d\n", sum);  
-    
+{
+    int result =arithmeticSum(1,100,1);
+    printf("%d\n",result);
     return 0;
 }
