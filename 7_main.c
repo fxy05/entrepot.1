e2bd63223a60b76714b85f4d2f9eeb8e22c394fd
fxy05 <fxy05.07.26@qq.com>
2025-11-04 21:05:41 +0800

    7_main.c

--- 7_main.c
+++ 7_main.c
@@ -1,28 +1,32 @@
 //202510304222
 //fxy05.07.26@qq.com
 //王鑫
+
 #include <stdio.h>
 
+// 数组反转函数
+void reverse_array(int arr[], int length) {
+    int temp;
+    for (int i = 0; i < length / 2; i++) {
+        temp = arr[i];
+        arr[i] = arr[length - 1 - i];
+        arr[length - 1 - i] = temp;
+    }
+}
+
 int main() {
-    int arr[5];
+    int arr[5] = {1, 2, 3, 4, 5};
     
+    // 调用反转函数
+    reverse_array(arr, 5);
     
-    for (int i = 0; i < 4; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-    
-    
-    for (int i = 4; i > 0; i--)
-    {
-       arr[i]=arr[i-1];
-    }
-   arr[0]=0;
-    
-    
-    for (int i = 0; i < 5; i++)
-    {
-        printf("%d ",arr[i]);
+    // 输出反转后的数组
+    for (int i = 0; i < 5; i++) {
+        if (i == 0) {
+            printf("%d", arr[i]);
+        } else {
+            printf(" %d", arr[i]);
+        }
     }
     printf("\n");
     
