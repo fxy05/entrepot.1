2e677aeed9b322ea049e29d8e2d88c3536cdefa2
fxy05 <fxy05.07.26@qq.com>
2025-11-04 21:47:42 +0800

    1_main.c

--- 1_main.c
+++ 1_main.c
@@ -2,27 +2,30 @@
 //fxy05.07.26@qq.com
 //王鑫
 #include <stdio.h>
-
-int main() {
+int main() 
+{
     int matrix[3][3];
-    int i, j;
-    
-    // 输入3×3矩阵
-    printf("请输入3×3矩阵的元素（按行输入）：\n");
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
+    printf("请输入3*3矩阵(每行3个数字,共3行):\n");
+    for (int i = 0; i < 3; i++) 
+    {
+        for (int j = 0; j < 3; j++) 
+        {
             scanf("%d", &matrix[i][j]);
         }
     }
-    
-    // 输出矩阵
-    printf("输出矩阵：\n");
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            printf("%d", matrix[i][j]);
+
+    printf("输出：\n");
+    for (int i = 0; i < 3; i++)
+        {
+        
+        printf("%d", matrix[i][0]);
+        
+        for (int j = 1; j < 3; j++) 
+        {
+            printf(" %d", matrix[i][j]);
         }
+        
         printf("\n");
     }
-    
     return 0;
 }
