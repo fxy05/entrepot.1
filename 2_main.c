35432d82f18214c1a1cc86cba3d5f935d200990e
fxy05 <fxy05.07.26@qq.com>
2025-11-20 21:22:59 +0800

    2_main.c

--- 2_main.c
+++ 2_main.c
@@ -1,32 +1,33 @@
-//202510304222
+//20251030422
 //fxy05.07.26@qq.com
 //王鑫
-#include<stdio.h>
-int main()
-{
-    int matrix[3][3];
-    printf("请输入3*3矩阵(每行3个数字,共3行):\n");
-    for(int i=0;i<3;i++)
-    {
-        for(int j=0;j<3;j++)
-        {
-            scanf("%d", &matrix[i][j]);
-        }
-    }
+#include <stdio.h>
 
-    printf("输出的3*3矩阵为:\n");
-   
-    for(int j=0;j<3;j++)
-    {
-        
-        printf("%d", matrix[0][j]);
+int main() 
+{
+    int arr[5];
+    int *p;
+    int i;
     
-        for(int i=1;i<3;i++)
-        {
-            printf(" %d", matrix[i][j]);
-        }
-       
-        printf("\n");
+    // 输入5个整数到数组arr
+    for(i = 0; i < 5; i++) 
+    {
+        scanf("%d", &arr[i]);
     }
+    
+    // 使用指针将数组中所有元素乘以2
+    p = arr;  // 指针指向数组首元素
+    for(i = 0; i < 5; i++) 
+    {
+        *(p + i) = *(p + i) * 2;
+    }
+    
+    // 打印处理后的数组
+    for(i = 0; i < 4; i++) 
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("%d\n", arr[4]);
+    
     return 0;
 }
