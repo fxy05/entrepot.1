7d2aede7052a6d8b409f5c26fbd97a8fe5cf7484
fxy05 <fxy05.07.26@qq.com>
2025-11-20 21:26:17 +0800

    7_main.c

--- 7_main.c
+++ 7_main.c
@@ -1,30 +1,51 @@
-//202510304222
+//20251030422
 //fxy05.07.26@qq.com
 //王鑫
 #include <stdio.h>
-void reverseArray(int arr[], int length) {
-    int left = 0;
-    int right = length - 1;
-    while (left < right) {
-        int temp = arr[left];
-        arr[left] = arr[right];
-        arr[right] = temp;
-        left++;
-        right--;
+
+// 使用指针实现冒泡排序
+void bubbleSort(int *arr, int n) 
+{
+    for (int i = 0; i < n - 1; i++) 
+    {
+        for (int j = 0; j < n - i - 1; j++) 
+        {
+            // 使用指针比较相邻元素
+            if (*(arr + j) > *(arr + j + 1)) 
+            {
+                // 交换元素
+                int temp = *(arr + j);
+                *(arr + j) = *(arr + j + 1);
+                *(arr + j + 1) = temp;
+            }
+        }
     }
 }
 
-int main() {
-    int arr[5];
-    for (int i = 0; i < 5; i++) {
-        scanf("%d", &arr[i]);
+int main() 
+{
+    int arr[10];
+    int *ptr = arr; // 定义指针指向数组
+    
+    printf("请输入10个整数（用空格分隔）: ");
+    
+    // 使用指针输入数据
+    for (int i = 0; i < 10; i++) 
+    {
+        scanf("%d", ptr + i);
     }
-    reverseArray(arr, 5);
-    for (int i = 0; i < 5; i++) {
-        printf("%d", arr[i]);
-        if (i < 4) {
-            printf(" ");
-        }
+    
+    // 调用排序函数
+    bubbleSort(arr, 10);
+    
+    printf("排序结果: ");
+    
+    // 使用指针输出排序后的结果
+    for (int i = 0; i < 10; i++) 
+    {
+        printf("%d ", *(ptr + i));
     }
+    printf("\n");
+    
     return 0;
 }
