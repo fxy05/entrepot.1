8ce6731843d41eb7d7974be4c71e26c88a0e94dd
fxy05 <fxy05.07.26@qq.com>
2025-11-20 21:39:29 +0800

    3_main.c

--- 3_main.c
+++ 3_main.c
@@ -1,35 +1,21 @@
 //202510304222
 //fxy05.07.26@qq.com
 //王鑫
+c
+   
+
 #include <stdio.h>
 
-// 使用指针作为参数实现两个整数的交换
-void swap(int *a, int *b) 
-{
+void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
-int main() 
-{
-    int num1, num2;
-    
-    
-    printf("请输入第一个整数: ");
-    scanf("%d", &num1);
-    
-    printf("请输入第二个整数: ");
-    scanf("%d", &num2);
-    
-
-    printf("交换前: num1 = %d, num2 = %d\n", num1, num2);
-    
-    
-    swap(&num1, &num2);
-    
-    
-    printf("交换后: num1 = %d, num2 = %d\n", num1, num2);
-    
+int main() {
+    int x, y;
+    scanf("%d %d", &x, &y);
+    swap(&x, &y);
+    printf("%d %d\n", x, y);
     return 0;
 }
