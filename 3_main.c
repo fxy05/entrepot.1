9b4431f35d2f9fc40c6e7b596aef08ae50744c80
fxy05 <fxy05.07.26@qq.com>
2025-11-20 21:23:33 +0800

    3_main.c

--- 3_main.c
+++ 3_main.c
@@ -1,38 +1,35 @@
-//202510304222
+//20251030422
 //fxy05.07.26@qq.com
 //王鑫
 #include <stdio.h>
-int main() {
-    int arr[10];
-    for (int i = 0; i < 10; i++) 
-    {
-        scanf("%d", &arr[i]);
-    }
-    for (int i = 0; i < 10 - 1; i++) 
-    {
-        for (int j = 0; j < 10 - 1 - i; j++) 
-        {
-            if (arr[j] > arr[j + 1]) 
-            {
-               
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-    for (int i = 0; i < 10; i++) 
-    {
-        if (i == 9) 
-        {
-            printf("%d", arr[i]);
-        } 
-        else 
-        {
-            printf("%d ", arr[i]);
-        }
-    }
-    printf("\n");
 
+// 使用指针作为参数实现两个整数的交换
+void swap(int *a, int *b) 
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() 
+{
+    int num1, num2;
+    
+    
+    printf("请输入第一个整数: ");
+    scanf("%d", &num1);
+    
+    printf("请输入第二个整数: ");
+    scanf("%d", &num2);
+    
+
+    printf("交换前: num1 = %d, num2 = %d\n", num1, num2);
+    
+    
+    swap(&num1, &num2);
+    
+    
+    printf("交换后: num1 = %d, num2 = %d\n", num1, num2);
+    
     return 0;
 }
